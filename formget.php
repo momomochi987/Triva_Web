499d50626f0edc3624b13c4fbd2755024ca9369c
as99us301 <as99us301@gmail.com>
2019-01-17 16:53:53 +0800

    完善表單功能

---
+++ formget.php
@@ -0,0 +1,15 @@
+以下是您所輸入的資料
+
+<?php 
+echo "您的姓名是";
+echo $_POST['name']."<br>"; 
+echo "您選擇的問題標題為";
+echo $_POST['qtitle']."<br>"; 
+echo "您的電子信箱為";
+echo $_POST['email']."<br>"; 
+echo "您的問題詳細敘述為";
+echo $_POST['detalii']."<br>"; 
+
+?>
+
+<a href="contactus.html">回上一頁</a>
